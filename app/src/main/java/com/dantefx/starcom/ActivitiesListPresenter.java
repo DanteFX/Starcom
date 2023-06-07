5fb75a3a317f98edfb0841e878506725b7e18fb0
JanDehuma <57647592+JanDehuma@users.noreply.github.com>
2023-06-06 19:29:06 -0600

    Update ActivitiesListPresenter.java

--- app/src/main/java/com/dantefx/starcom/ActivitiesListPresenter.java
+++ app/src/main/java/com/dantefx/starcom/ActivitiesListPresenter.java
@@ -1,6 +1,7 @@
 package com.dantefx.starcom;
 
 import android.database.Cursor;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,6 +23,7 @@ public class ActivitiesListPresenter extends Fragment {
     private Administra administra;
     private RecyclerView recyclerView;
     private TareasAdapter adaptador;
+    private Cursor cursor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,6 +36,10 @@ public class ActivitiesListPresenter extends Fragment {
         adaptador = new TareasAdapter(cursor);
         recyclerView.setAdapter(adaptador);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Configurar el GridLayoutManager con 7 columnas (mismo número que las columnas en el TableLayout)
+        /*GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 7);
+        recyclerView.setLayoutManager(layoutManager);*/
 
         return binding.getRoot();
     }
@@ -50,12 +57,20 @@ public class ActivitiesListPresenter extends Fragment {
         });
     }
 
+    public void actualizarLista() {
+        cursor = administra.obtenerTareas();
+        adaptador.swapCursor(cursor);
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
 }
+
+
+
 
 
 
